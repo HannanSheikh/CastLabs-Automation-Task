cbcff241edb7bf69ef3fdb0af10f631f734b2d92
Abdul Hannan <hannan.sheikh2526@gmail.com>
2022-11-23 09:30:45 +0100

    Final commit

--- cypress/e2e/castLabsTest.cy.js
+++ cypress/e2e/castLabsTest.cy.js
@@ -2,24 +2,23 @@
 
  describe('Recruitment Test Cast Labs',()=>{
     it('Video Functionality test',()=>{
+        // open the web page
         cy.visit('https://demo.castlabs.com/')
+        //select the HLS video
         cy.contains('HLS').click()
 
         cy.wait(10000)
+        //seek the video for 70% 
         cy.get('.pp-ui-overlay.pp-ui-stretch.pp-ui-hidden > div > div').click({force:true})
         cy.get(".pp-ui-bar-range-thumb.pp-ui-bar-range-thumb-background")
         .invoke('attr', 'style', 'left: 70%;')
         .should('have.attr', 'style', 'left: 70%;')
-        // .click();
-
-        cy.visit('https://demo.castlabs.com/')
-        cy.contains('HLS').click()
-        cy.get('.pp-ui-overlay.pp-ui-stretch.pp-ui-hidden > div > div').click({force:true})
-        cy.get(".pp-ui-button-22.pp-ui-button.pp-ui-button-options").click()
-         cy.contains(" Video ").click()
-
-        cy.get("div[class='pp-ui-options-selection'] div:nth-child(2)").click({force:true})
-       
-    })       
-   
+        
+        //Clicking on 3 dots button to change resolution
+        cy.get("[class='pp-ui-button-22 pp-ui-button pp-ui-button-options']").click()
+        cy.get("[class='pp-ui-clickable']").eq(3).then(videobutton=>{
+        cy.wrap(videobutton).click()
+        cy.wrap(videobutton).find("span").should("contain"," 960x540 @ 2.48 Mbps " )
+        })
+  }) 
 })
